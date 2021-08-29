32f48e71f43518c31e23575413f47266774ee043
ArnonAsquira <arnonafking2010@gmail.com>
2021-08-29 17:21:08 +0300

    changing var to let

--- js exc #1.js
+++ js exc #1.js
@@ -2,7 +2,7 @@ const num = 725;
 let sum = 0;
 // your code goes here
 let numAsString = num.toString();
-for(var i = 0; i < numAsString.length; i++){
+for(let i = 0; i < numAsString.length; i++){
     sum += parseInt(numAsString[i],10);
 }
 console.log(sum);
